0d1da7361979ae4a21d3a4df9a4d2499c398cccd
MityaAndreevich <dmitry.logachev@icloud.com>
2021-08-29 00:38:33 +0300

    Redone loginTestApp with new names

---
+++ AboutMeApp/LogInViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Дмитрий Логачёв on 28.08.2021.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    // MARK: - Private Properties
+    private let realUserName = "Name"
+    private let realUserPassword = "Password"
+    //MARK: - Override Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        userNameTextField.layer.cornerRadius = 15
+        passwordTextField.layer.cornerRadius = 15
+    }
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userNameTextField.text
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func forgotRegisteredData(_ sender: UIButton) {
+        sender.tag == 0
+            ? sendingAlertMessage(title: "Forgot user Name?", message: "User name: \(realUserName) 😉")
+            : sendingAlertMessage(title: "Forgot password?", message: "Password: \(realUserPassword) 😉")
+    }
+    
+    @IBAction func logInButtonPressed() {
+        guard
+            userNameTextField.text == realUserName,
+            passwordTextField.text == realUserPassword
+        else {
+            sendingAlertMessage(
+                title: "Alert!",
+                message: "Wrong User Name or Password"
+            )
+            passwordTextField.text = ""
+            return
+        }
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    // MARK: - Private Methods
+        private func sendingAlertMessage(title: String, message: String) {
+            let alertMessage = UIAlertController(
+                title: title,
+                message: message,
+                preferredStyle: .alert
+            )
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alertMessage.addAction(okAction)
+            present(alertMessage, animated: true)
+        }
+}
+
+extension LogInViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInButtonPressed()
+            performSegue(withIdentifier: "showWelcomeVC", sender: nil)
+        }
+        return true
+    }
+}
